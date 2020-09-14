1ba2877ff118e7df687f124d22c1727b9da59bf4
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-09-14 16:24:52 +0900

    Use git ls-files

--- clang-format/check-format.sh
+++ clang-format/check-format.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-set -eux
-clang-format -i $(find $(pwd) -name "*.[ch]pp" -or -name "*.[ch]")
-git diff --exit-code --ignore-submodule=all
+set -eu
+
+FILES=$(git ls-files | grep -e ".*.[ch]pp")
+if [[ -z "$FILES" ]]; then
+  echo "No C++ codes found"
+  exit 1
+fi
+
+clang-format -i $FILES
+git diff --exit-code --ignore-submodules=all $FILES
