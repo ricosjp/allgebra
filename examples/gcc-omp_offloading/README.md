eade5736daa87bc97bfd9f429ccf5e22068e2442
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2020-11-01 01:01:02 +0900

    gcc omp offloadng

---
+++ examples/gcc-omp_offloading/README.md
@@ -0,0 +1,8 @@
+# OpenACC Example for C++
+
+|Example                           | Description                         |
+|:---------------------------------|:------------------------------------|
+|[acc.cpp](./acc.cpp)              | Minimal example for testing OpenACC |
+|[acc_cublas.cpp](./acc_cublas.cpp)| Use OpenACC with CUDA libraries     |
+
+See comments in [Makefile](./Makefile) and [CMakeLists.txt](./CMakeLists.txt) for detail.
---
+++ examples/gcc-omp_offloading/omp_offloading_cublas.cpp
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2020 RICOS Co. Ltd.
+ *
+ * This file is a part of ricosjp/allgebra, distributed under Apache-2.0 License
+ * https://github.com/ricosjp/allgebra
+ */
+
+#include <cstdlib> // atoi, malloc
+#include <iostream>
+
+int main(int argc, char **argv) {
+  int size = 0;
+  if (argc == 2) {
+    size = std::atoi(argv[1]);
+  } else {
+    std::cout << "error, $1 is vector size" << std::endl;
+    return 1;
+  }
+
+  double *x = (double *)malloc(sizeof(double) * size);
+  double *y = (double *)malloc(sizeof(double) * size);
+
+  for (int i = 0; i < size; i++) {
+    x[i] = 1.0;
+    y[i] = 2.0;
+  }
+
+  double dot = 0.0;
+
+  cublasHandle_t h;
+  check(cublasCreate(&h));
+
+#pragma omp target data use_device_ptr(x, y)
+  dot = cublasDdot(size, x, 1, y, 1);
+
+  cublasDestroy(h);
+
+  if (dot != 2.0 * size) {
+    std::cout << "dot = " << dot << std::endl;
+    std::cout << "error!" << std::endl;
+    return 1;
+  } else {
+    std::cout << "dot = " << dot << std::endl;
+    std::cout << "Pass!" << std::endl;
+    return 0;
+  }
+}
