1dab26c6f6c212a685cd8d788f0d9189e01b62c6
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-12-22 16:33:46 +0900

    Add glue Makefile

---
+++ clang11gcc7.mk
@@ -0,0 +1,22 @@
+PUBLIC_REGISTRY    := ghcr.io/ricosjp/allgebra
+CI_REGISTRY_IMAGE  ?= registry.ritc.jp/ricos/allgebra
+CI_COMMIT_REF_NAME ?= manual_deploy
+
+TARGETS := cuda10_1 cuda10_1/clang11gcc7 cuda10_1/clang11gcc7/mkl cuda10_1/clang11gcc7/oss
+
+.PHONY: $(TARGETS)
+
+cuda10_1:
+	$(MAKE) -C $@
+
+cuda10_1/clang11gcc7: cuda10_1
+	$(MAKE) -C $@
+
+cuda10_1/clang11gcc7/mkl: cuda10_1/clang11gcc7
+	$(MAKE) -C $@
+
+cuda10_1/clang11gcc7/oss: cuda10_1/clang11gcc7
+	$(MAKE) -C $@
+
+push: $(TARGETS)
+	$(foreach TARGET,$(TARGETS),docker push $(CI_REGISTRY_IMAGE)/$(TARGET):$(CI_COMMIT_REF_NAME);)
