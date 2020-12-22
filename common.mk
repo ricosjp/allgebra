98f45fee05f90ea40987d56eaa7aa6900fd3b3e9
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-12-22 17:12:49 +0900

    Add annotation and examples/

---
+++ common.mk
@@ -0,0 +1,13 @@
+# Copyright 2020 RICOS Co. Ltd.
+#
+# This file is a part of ricosjp/allgebra, distributed under Apache-2.0 License
+# https://github.com/ricosjp/allgebra
+#
+
+PUBLIC_REGISTRY    := ghcr.io/ricosjp/allgebra
+CI_REGISTRY_IMAGE  ?= registry.ritc.jp/ricos/allgebra
+CI_COMMIT_REF_NAME ?= manual_deploy
+
+ALLGEBRA_VERSION := 20.11.0
+GIT_HASH         := $(shell git rev-parse HEAD)
+BUILD_DATE       := $(shell date --rfc-3339=ns)
