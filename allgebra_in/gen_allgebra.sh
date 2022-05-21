a4ff309f114e566d4ef4db804e25c99e8415161b
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2022-05-22 02:57:55 +0900

    gen cuda dockerfile

--- allgebra_in/gen_allgebra.sh
+++ allgebra_in/gen_allgebra.sh
@@ -20,32 +20,32 @@ rm -rf ./cuda${CUDA_MAJOR}_${CUDA_MINOR}
 # cuda clang mkl
 TARGET_DIR=cuda${CUDA_MAJOR}_${CUDA_MINOR}/clang${CLANG_MAJOR}/mkl
 mkdir -p $TARGET_DIR
-TARGET_COMPILER=clang; eval "echo \"$(cat ./Dockerfile.mkl)\"" > $TARGET_DIR/Dockerfile
+TARGET_COMPILER=clang; eval "echo \"$(cat ./Dockerfile.mkl.in)\"" > $TARGET_DIR/Dockerfile
 ALLGEBRA_TARGET=${TARGET_DIR}; eval "echo \"$(cat ./Makefile.in)\"" > $TARGET_DIR/Makefile
 
 # cuda clang oss
 TARGET_DIR=cuda${CUDA_MAJOR}_${CUDA_MINOR}/clang${CLANG_MAJOR}/oss
 mkdir -p $TARGET_DIR
-TARGET_COMPILER=clang; eval "echo \"$(cat ./Dockerfile.oss)\"" > $TARGET_DIR/Dockerfile
+TARGET_COMPILER=clang; eval "echo \"$(cat ./Dockerfile.oss.in)\"" > $TARGET_DIR/Dockerfile
 ALLGEBRA_TARGET=${TARGET_DIR}; eval "echo \"$(cat ./Makefile.in)\"" > $TARGET_DIR/Makefile
 
 # cuda gcc mkl
 TARGET_DIR=cuda${CUDA_MAJOR}_${CUDA_MINOR}/gcc${GCC_MAJOR}/mkl
 mkdir -p $TARGET_DIR
-TARGET_COMPILER=gcc; eval "echo \"$(cat ./Dockerfile.mkl)\"" > $TARGET_DIR/Dockerfile
+TARGET_COMPILER=gcc; eval "echo \"$(cat ./Dockerfile.mkl.in)\"" > $TARGET_DIR/Dockerfile
 ALLGEBRA_TARGET=${TARGET_DIR}; eval "echo \"$(cat ./Makefile.in)\"" > $TARGET_DIR/Makefile
 
 # cuda gcc oss
 TARGET_DIR=cuda${CUDA_MAJOR}_${CUDA_MINOR}/gcc${GCC_MAJOR}/oss
 mkdir -p $TARGET_DIR
-TARGET_COMPILER=gcc; eval "echo \"$(cat ./Dockerfile.oss)\"" > $TARGET_DIR/Dockerfile
+TARGET_COMPILER=gcc; eval "echo \"$(cat ./Dockerfile.oss.in)\"" > $TARGET_DIR/Dockerfile
 ALLGEBRA_TARGET=${TARGET_DIR}; eval "echo \"$(cat ./Makefile.in)\"" > $TARGET_DIR/Makefile
 
 # cuda clang
 # cuda gcc
 
 # cuda
-TARGET_DIR=cuda${CUDA_MAJOR}_${CUDA_MINOR}/
+TARGET_DIR=cuda${CUDA_MAJOR}_${CUDA_MINOR}
 mkdir -p $TARGET_DIR
-eval "echo \"$(cat ./Dockerfile.cuda)\"" > Dockerfile
+eval "echo \"$(cat ./Dockerfile.cuda.in)\"" > ${TARGET_DIR}/Dockerfile
 ALLGEBRA_TARGET=${TARGET_DIR}; eval "echo \"$(cat ./Makefile.in)\"" > $TARGET_DIR/Makefile
