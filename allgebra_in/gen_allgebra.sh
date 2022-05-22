d02d7aeb47924ed2cfcd8bd53b0de2392e132239
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2022-05-22 10:41:48 +0900

    gen Makefile

--- allgebra_in/gen_allgebra.sh
+++ allgebra_in/gen_allgebra.sh
@@ -42,6 +42,12 @@ TARGET_COMPILER=gcc; eval "echo \"$(cat ./Dockerfile.oss.in)\"" > $TARGET_DIR/Do
 ALLGEBRA_TARGET=${TARGET_DIR}; eval "echo \"$(cat ./Makefile.in)\"" > $TARGET_DIR/Makefile
 
 # cuda clang
+TARGET_DIR=cuda${CUDA_MAJOR}_${CUDA_MINOR}/clang${CLANG_MAJOR}
+mkdir -p $TARGET_DIR
+#eval "echo \"$(cat ./Dockerfile.clang.in)\"" > $TARGET_DIR/Dockerfile
+eval "echo \"$(cat ./Dockerfile.clang.in)\"" > $TARGET_DIR/Dockerfile
+ALLGEBRA_TARGET=${TARGET_DIR}; eval "echo \"$(cat ./Makefile.in)\"" > $TARGET_DIR/Makefile
+
 # cuda gcc
 
 # cuda
